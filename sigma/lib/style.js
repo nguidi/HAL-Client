9244f0835d98b7ea77e28fde36d76307ea4e99b8
nguidi <neri3004@gmail.com>
2013-06-25 18:34:20 -0300

    Plugins + Bootsrap new site

--- sigma/lib/style.js
+++ sigma/lib/style.js
@@ -3,13 +3,13 @@ var	use_less=false
 	(use_less)
 		?steal(	
 			'steal/less'
-		,	'bootstrap/bootstrap/less/bootstrap.less'
-		,	'bootstrap/bootstrap/less/responsive.less'
+		,	'sigma/plugins/bootstrap/less/bootstrap.less'
+		,	'sigma/plugins/bootstrap/less/responsive.less'
 		,	'font-awesome/less/font-awesome.less'
 		)
 		:steal(	
-			'bootstrap/bootstrap/css/bootstrap.css'
-		,	'bootstrap/bootstrap/css/bootstrap-responsive.css'
+			'sigma/plugins/bootstrap/css/bootstrap.css'
+		,	'sigma/plugins/bootstrap/css/bootstrap-responsive.css'
 		,	'font-awesome/css/font-awesome.css'
 		)
 )
